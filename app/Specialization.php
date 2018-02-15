9444f666cb6daff8268cf225fbad09bb9705f1e0
Daniel Bajana <it.danielbajana@gmail.com>
2018-02-15 10:21:21 +0800

    Add nationality and physician model

---
+++ app/Specialization.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Specialization extends Model
+{
+    protected $table = 'specializations';
+    protected $guarded = [];
+    public $timestamps = false;
+}
