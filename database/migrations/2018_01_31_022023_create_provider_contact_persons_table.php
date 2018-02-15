aaff6b4a397853a5284c8e41fcd4f3ed05967c73
Daniel Bajana <it.danielbajana@gmail.com>
2018-02-15 10:23:46 +0800

    Fix migrate refresh error

--- database/migrations/2018_01_31_022023_create_provider_contact_persons_table.php
+++ database/migrations/2018_01_31_022023_create_provider_contact_persons_table.php
@@ -34,6 +34,6 @@ class CreateProviderContactPersonsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('provider_contact_details');
+        Schema::dropIfExists('provider_contact_persons');
     }
 }
